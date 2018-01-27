07cfaad22bdb7934e275f261e778e3ac706823ca
Driver Station <bhsrobotix2876-programming@gmail.com>
2018-01-27 15:40:49 -0500

    Fix issues with elevator and intake subsystems
    - missing requires subsystem in elevator
    - elevator stop command needed to set the motor to zero in execute to
    get rid of motor safety errors

--- src/org/usfirst/frc2876/PowerUp2018/commands/IntakeStop.java
+++ src/org/usfirst/frc2876/PowerUp2018/commands/IntakeStop.java
@@ -44,7 +44,7 @@ public class IntakeStop extends Command {
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
-    	
+    	Robot.intake.setBothMotors(0);
     }
 
     // Make this return true when this Command no longer needs to run execute()
