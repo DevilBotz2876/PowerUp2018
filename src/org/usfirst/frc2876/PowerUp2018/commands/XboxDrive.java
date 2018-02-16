88f809833c29546f0687e5104ac9c2176cf66187
Maciej Wiczynski <loafjunk@loafdog.org>
2018-02-15 22:36:01 -0500

    Add command to hold elevator

--- src/org/usfirst/frc2876/PowerUp2018/commands/XboxDrive.java
+++ src/org/usfirst/frc2876/PowerUp2018/commands/XboxDrive.java
@@ -54,17 +54,17 @@ public class XboxDrive extends Command {
 //    		Robot.driveTrain.setVelocityArcadeJoysticks(0, -1);
 //    		Robot.driveTrain.arcadeDrive(0,0);
     		
-    		if(Math.abs(Robot.oi.getRightX()) <= .1 && !(Math.abs(Robot.oi.getLeftY()) <= .1)){
-    			if(isStraightBeginning){
-    				Robot.driveTrain.startStraight();
-    				isStraightBeginning = false;
-    			}
-    			Robot.driveTrain.setStraightVelocityArcadeJoysticks(Robot.oi.getLeftY());
-    		}else{
-    			isStraightBeginning = true;
-    			Robot.driveTrain.stopStraight();
-    			Robot.driveTrain.setVelocityArcadeJoysticks(Robot.oi.getRightX(), Robot.oi.getLeftY());
-    		}
+//    		if(Math.abs(Robot.oi.getRightX()) <= .1 && !(Math.abs(Robot.oi.getLeftY()) <= .1)){
+//    			if(isStraightBeginning){
+//    				Robot.driveTrain.startStraight();
+//    				isStraightBeginning = false;
+//    			}
+//    			Robot.driveTrain.setStraightVelocityArcadeJoysticks(Robot.oi.getLeftY());
+//    		}else{
+//    			isStraightBeginning = true;
+//    			Robot.driveTrain.stopStraight();
+//    			Robot.driveTrain.setVelocityArcadeJoysticks(Robot.oi.getRightX(), Robot.oi.getLeftY());
+//    		}
     	}
     }
 
